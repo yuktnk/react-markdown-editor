0d0dd068d8bc9e9d2a320c9647c6cb3569960135
Yuki Tanaka <yuki_tanaka@commude.co.jp>
2021-06-03 18:27:42 +0900

    TypeScriptの導入

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
+  module: {
+    rules: [ // webpack に対してビルド時に追加で行う処理を記述
+      {
+        test: /\.ts$/,           //.ts で終わるファイルに対して
+        use: 'ts-loader',        // ts-loader を実行する
+        exclude: /node_modules/, // 除外するファイルを正規表現で指定
+      },
+    ],
+  },
+  resolve: { // モジュールとして解決するファイルの拡張子を指定
+    extensions: ['.ts'],
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',

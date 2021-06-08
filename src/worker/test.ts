7a34daf4cdc873fe480f354cf77268681205e01f
yuktnk <yuki0wl@gmail.com>
2021-06-08 09:32:32 +0900

    Web Worker導入

---
+++ src/worker/test.ts
@@ -0,0 +1,7 @@
+// 型チェックを回避
+const worker: Worker = self as any;
+
+worker.addEventListener("message", (event) => {
+  console.log("Worker Received:", event.data);
+  worker.postMessage({ result: event.data });
+});
